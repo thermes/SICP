809c31a743e479e404197e8545473838a128a9d8
HORINOUCHI Masato <thermes@confei.to>
2014-01-21 12:38:54 +0900

    typo.

--- 2/ex.2.52.scm
+++ 2/ex.2.52.scm
@@ -26,7 +26,7 @@
 
 
 ;; c
-(define (square-of-four tl tr bl tr)
+(define (square-of-four tl tr bl br)
   (lambda (painter)
     (let ((top (beside (tl painter) (tr painter)))
           (bottom (beside (bl painter) (br painter))))
